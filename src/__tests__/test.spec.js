61059da1c5b020ddf3aae9567995841b291ed916
Gulacsy-Toth <tompascall@gmail.com>
2017-01-29 02:35:25 +0100

    check driver instance when getting raw entries

--- src/__tests__/test.spec.js
+++ src/__tests__/test.spec.js
@@ -1,5 +1,50 @@
 import weblog from '../weblog';
+jest.unmock('../weblog');
 
-test('get weblog', () => {
-  expect({}.toString.call(weblog)).toBe('[object Object]');
+describe('weblog', () => {
+
+  const mockDriver = {
+    manage () { return this },
+    logs () { return this },
+    get () {return this }
+  };
+
+  it('it is an object', () => {
+    expect({}.toString.call(weblog)).toBe('[object Object]');
+  });
+  
+  describe('checkDriver', () => {
+    it('should throw err if not driver given or not set up properly', () => {
+      let fakeCallWithoutDriver = () => {
+        weblog.checkDriver();
+      };
+      let fakeCallWithDriver = () => {
+        weblog.checkDriver({ driver: mockDriver});
+      };
+
+      expect(fakeCallWithoutDriver).toThrow();
+      expect(fakeCallWithDriver).not.toThrow();
+    });
+  });
+  
+  describe('getRawEntries', () => {
+    it('it is defined', () => {
+      expect(weblog.getRawEntries).toBeDefined();
+    });
+
+    it('it needs a driver instance', () => {
+      expect(weblog.getRawEntries).toThrow();
+      let fakeCall = () => {
+        weblog.getRawEntries({
+          driver : mockDriver 
+        });
+      }
+      expect(fakeCall).not.toThrow();
+    });
+    
+    it('it gives back a thenable', () => {
+      expect(typeof weblog.getRawEntries({driver : mockDriver}).then).toEqual('function');
+    });
+  });
+
 });
--- src/weblog.js
+++ src/weblog.js
@@ -1,4 +1,19 @@
 const weblog = {
+  checkDriver ({ driver } = {}) {
+    try {
+      if (typeof driver.manage().logs().get !== 'function') {
+        throw Error();
+      }
+    }
+    catch (e) {
+      throw Error('driver has not been set up properly, cannot log network traffic');
+    }
+  },
+
+  getRawEntries ({ driver } = {}) {
+    this.checkDriver({ driver });
+    return Promise.resolve();
+  }
 };
 
 export default weblog;
