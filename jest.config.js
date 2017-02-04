9a7a0a8bec3bdbb5f1843d17dfe346ad1782f4f1
Gulacsy-Toth <tompascall@gmail.com>
2017-02-04 01:43:20 +0100

    update config files

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,3 @@
 {
-  "bail": true,
   "verbose": true
 }
--- webpack.spike.config.js
+++ webpack.spike.config.js
@@ -18,11 +18,6 @@ module.exports = {
     loaders: [
     {
         test: /\.js$/,
-        // include: [
-        //   path.resolve(__dirname, "src"),
-        //   path.resolve(__dirname, "spike"),
-        //   path.resolve(__dirname, "driver")
-        // ],
         exclude: /node_modules/,
 
         loader: "babel-loader",
@@ -30,47 +25,19 @@ module.exports = {
         options: {
           presets: ["es2015"]
         },
-        // options for the loader
       }
     ]
   },
-  // resolveLoader: {
-  //         modulesDirectories: [
-  //             path.resolve(__dirname, "node_modules"),
-  //                 ]
-  // },
-  // resolve: {
-  //   // options for resolving module requests
-  //   // (does not apply to resolving to loaders)
-
-  //   root: [
-  //     path.resolve(__dirname, "src"),
-  //     path.resolve(__dirname, "spike"),
-  //     path.resolve(__dirname, "driver"),
-  //   ],
-  //   // directories where to look for modules
-  //   // moduleDirectories: ['node_modules'], 
-
-  //   extensions: [".js", ".json", ".jsx"],
-  //   // extensions that are used
-  // },
 
   performance: {
-    hints: "warning", // enum
+    hints: "warning", 
   },
 
-  devtool: "source-map", // enum
-  // enhance debugging by adding meta info for the browser devtools
-  // source-map most detailed at the expense of build speed.
+  devtool: "source-map",
 
   context: __dirname, // string (absolute path!)
-  // the home directory for webpack
-  // the entry and module.rules.loader option
-  //   is resolved relative to this directory
 
   target: "node", // enum
-  // the environment in which the bundle should run
-  // changes chunk loading behavior and available modules
 
   stats: {
     /* TODO */
@@ -83,5 +50,4 @@ module.exports = {
   plugins: [
     // ...
   ],
-  // list of additional plugins
 }
